f46ba0d204545bab3ac53d34a3c84b88c5c4b15d
ohikouta <c360032@st.cuc.ac.jp>
2025-11-22 23:29:23 +0900

    refactor:HomeViewに合わせてルート修正

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import ChatView from '../views/ChatView.vue';
+import HomeView from '../views/HomeView.vue';
 import PrivateChatView from '../views/PrivateChatView.vue';
 import UserList from '../components/UserList.vue';
 import RegisterComponent from '../components/RegisterComponent.vue';
@@ -7,13 +7,12 @@ import LoginComponent from '../components/LoginComponent.vue';
 import ProfileComponent from '../components/ProfileComponent.vue';
 
 const routes = [
-  { path: '/', name: 'Chat', component: ChatView },
+  { path: '/', name: 'Home', component: HomeView },
   { path: '/register', component: RegisterComponent },
   { path: '/login', component: LoginComponent },
   { path: '/profile', component: ProfileComponent },
   { path: '/users', name: 'UserList', component: UserList },
   { path: '/chat/:userId', name: 'PrivateChat', component: PrivateChatView }
-  // 他のルートがあればここに追加
 ];
 
 const router = createRouter({
