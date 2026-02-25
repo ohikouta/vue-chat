e5ad1b4631aa9ff7050e965805a9ca88374f8fea
ohikouta <c360032@st.cuc.ac.jp>
2026-02-25 22:14:09 +0900

    feat: add global header and app background

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
+    <Header />
     <router-view />
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+
 export default {
-  name: 'App'
-}
+  name: 'App',
+  components: {
+    Header
+  }
+};
 </script>
 
 <style>
@@ -17,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   min-height: 100vh;
+  background: #f7f8fb;
 }
 </style>
-
